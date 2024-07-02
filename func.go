0b03d92effae658a0dacb7e9a2c0bb70fe949bbe
Shreyansh Pandey <shreyansh487@gmail.com>
2024-07-03 02:48:28 +0530

    variables, constants and fmt package

---
+++ func.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func PrintValue(){ // function name should start with a capital letter if we want to access it from another package(public). Otherwise, it will be private to the package
+	fmt.Print(url)
+}
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+var url = "https://www.google.com" // package level variable or global variable
+
 func main(){
-	print("Hello, world!")
+	//function scope variables
+	var message string = "Hello, world!" // when we declare a variable, we need to specify the type, but when we initialize it, we can omit the type
+	var price = 24 // type inference will automatically assign the type of the value
+	title := "Golang Programming" // without using var keyword we can declare and initialize a variable using :=
+	const maxSpeed int = 100 // constant variable
+	print(message, price, title, maxSpeed, url)
+	PrintValue() 
 }
